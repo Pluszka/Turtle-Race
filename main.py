d594498a498229a4f560301ce4a61d29b67ef418
Pliszka <karoliniakkjk@gmail.com>
2022-01-25 19:22:05 +0100

    Only correct input

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 import random
-import turtle
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=500, height=400)
-race_on = False
+COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 
-user_bet = screen.textinput(title='Your bet', prompt='Choose the color of your turtle:')
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-all_turtles = []
+def correct_input():
+    user_bet = None
+    while not user_bet in COLORS:
+        user_bet = screen.textinput(title='Your bet',
+        prompt=f'Choose the color of your turtle:\n{COLORS}\n')
+    return user_bet
 
-for turtle_index in range(0, 6):
-    Andy = Turtle(shape='turtle')
-    Andy.color(colors[turtle_index])
-    Andy.penup()
-    Andy.goto(x=-220, y= 150- turtle_index * 60)
-    all_turtles.append(Andy)
 
+def creating_turts():
+    turtles = []
+
+    for turtle_index in range(0, 6):
+        Andy = Turtle(shape='turtle')
+        Andy.color(COLORS[turtle_index])
+        Andy.penup()
+        Andy.goto(x=-220, y= 150- turtle_index * 60)
+        turtles.append(Andy)
+    return turtles
 
 def race():
-    if user_bet:
-        race_on = True
+    race_on = True
     while race_on:
         for turtle in all_turtles:
             if turtle.xcor() > 230:
-                winner = turtle.color()
+                winner = turtle.color()[0]
                 race_on = False
             distance = random.randint(0, 10)
             turtle.forward(distance)
     return winner
 
-win = race()[0]
-if win == user_bet:
-    print('You won!')
-else:
-    print(f'You lose!')
-print(f'The {win} turtle is the winner!')
+
+def checking(win, user_bet):
+    if win == user_bet:
+        print('You won!')
+    else:
+        print(f'You lose!')
+    print(f'The {win} turtle is the winner!')
+
+all_turtles = creating_turts()
+checking(correct_input(), race())
+
 
 
 screen.exitonclick()
